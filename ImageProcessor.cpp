5eda365df2d608cc01560eb3bb6c379127d4224a
Glenn Butcher <glenn.butcher@gmail.com>
2019-01-18 23:09:45 -0700

    more work

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -1,9 +1,11 @@
 #include "ImageProcessor.hpp"
 
 #include <opencv2/imgproc.hpp>
+#include <math.h>
+
 
 struct pix {
-	float r, g, b;
+	float b, g, r;
 };
 
 void ImageProcessor::applyBlur(unsigned kernelsize)
@@ -13,21 +15,27 @@ void ImageProcessor::applyBlur(unsigned kernelsize)
 
 void ImageProcessor::applyLog()
 {
-	cv::log( image, image);
+	pix * src = (pix *) image.data;
+	double a = 0.17883277, b = 0.28466892, c = 0.55991073;
+	double rubicon = 1.0/12.0;
+	double sqrt3 = sqrt(3);
+
+	for (unsigned i=0; i< image.rows*image.cols; i++) {
+		if (src[i].r > 0.0) if (src[i].r > rubicon) src[i].r = a * log(12*src[i].r - b) + c; else src[i].r = sqrt3 * pow(src[i].r, 0.5); 
+		if (src[i].g > 0.0) if (src[i].g > rubicon) src[i].g = a * log(12*src[i].g - b) + c; else src[i].g = sqrt3 * pow(src[i].g, 0.5); 
+		if (src[i].b > 0.0) if (src[i].b > rubicon) src[i].b = a * log(12*src[i].b - b) + c; else src[i].b = sqrt3 * pow(src[i].b, 0.5); 
+	}
 }
 
 void ImageProcessor::applyDemosaic()
 {
-	unsigned w = image.cols;  //image.step?
+	unsigned w = image.cols;
 	unsigned h = image.rows;
 
 	cv::Mat H(image.rows/2, image.cols/2, CV_32FC3);
 
-	pix * halfimage = (pix *) H.data;
-	pix * img = (pix *) image.data;
-
-	
-	
+	pix * halfimage = (pix *) H.data;  //float RGB destination, half-sized
+	float * img = (float *) image.data; //single-channel float 
 
 	std::vector<unsigned> q = {0, 1, 1, 2};  //default pattern is RGGB, where R=0, G=1, B=2
 	//if (imginfo["LibrawCFAPattern"] == "GRBG") q = {1, 0, 2, 1};
@@ -35,8 +43,8 @@ void ImageProcessor::applyDemosaic()
 	//if (imginfo["LibrawCFAPattern"] == "BGGR") q = {2, 1, 1, 0};
 
 
-	for (unsigned y=0; y<h; y+=2) {
-		for (unsigned x=0; x<w; x+=2) {
+	for (unsigned y=0; y<h-2; y+=2) {
+		for (unsigned x=0; x<w-2; x+=2) {
 			unsigned Hpos = (x/2) + (y/2)*(w/2);
 			float pix[3] = {0.0, 0.0, 0.0};
 			unsigned pos[4];
@@ -45,7 +53,8 @@ void ImageProcessor::applyDemosaic()
 			pos[2] = x + (y+1)*w; //lower left
 			pos[3] = (x+1) + (y+1)*w;  //lower right
 			for (unsigned i=0; i<q.size(); i++) 
-				pix[q[i]] += img[pos[i]].r;  //use r, in grayscale, they're all the same...
+				pix[q[i]] += 
+					img[pos[i]]; //.r;  //use r, in grayscale, they're all the same...
 			pix[1] /= 2.0;
 			halfimage[Hpos].r = pix[0];
 			halfimage[Hpos].g = pix[1];
--- gimg.cpp
+++ gimg.cpp
@@ -3,8 +3,73 @@
 
 #include <opencv2/highgui.hpp>
 
-//using namespace cv;
-//using namespace std;
+//https://github.com/ccxvii/asstools/blob/master/getopt.c
+/*
+ * This is a version of the public domain getopt implementation by
+ * Henry Spencer originally posted to net.sources.
+ *
+ * This file is in the public domain.
+ */
+
+
+#define getopt xgetopt
+#define optarg xoptarg
+#define optind xoptind
+
+char *optarg; /* Global argument pointer. */
+int optind = 0; /* Global argv index. */
+
+static char *scan = NULL; /* Private scan pointer. */
+
+int
+getopt(int argc, char *argv[], char *optstring)
+{
+	char c;
+	char *place;
+
+	optarg = NULL;
+
+	if (!scan || *scan == '\0') {
+		if (optind == 0)
+			optind++;
+
+		if (optind >= argc || argv[optind][0] != '-' || argv[optind][1] == '\0')
+			return EOF;
+		if (argv[optind][1] == '-' && argv[optind][2] == '\0') {
+			optind++;
+			return EOF;
+		}
+
+		scan = argv[optind]+1;
+		optind++;
+	}
+
+	c = *scan++;
+	place = strchr(optstring, c);
+
+	if (!place || c == ':') {
+		fprintf(stderr, "%s: unknown option -%c\n", argv[0], c);
+		return '?';
+	}
+
+	place++;
+	if (*place == ':') {
+		if (*scan != '\0') {
+			optarg = scan;
+			scan = NULL;
+		} else if( optind < argc ) {
+			optarg = argv[optind];
+			optind++;
+		} else {
+			fprintf(stderr, "%s: option requires argument -%c\n", argv[0], c);
+			return ':';
+		}
+	}
+
+	return c;
+}
+
+//end getopt.c
 
 void err(std::string msg) {
 	printf("Error: %s\n", msg.c_str());
@@ -20,34 +85,47 @@ int main( int argc, char** argv )
 	
 	if (argc < 2) err("Need at least a filename.\n");
 
-	std::vector<std::string> inp = split(argv[1], ":");
+	int f;
+	opterr = 0;
+	while ((f = getopt(argc, argv, (char *) "d")) != -1)
+		switch(f) {
+			case 'd':  //display each step
+				display = true;
+				break;
+			default:
+				exit(-1);
+
+		}
+
+
+	std::vector<std::string> inp = split(argv[optind], ":"); optind++;
 	imageName = inp[0];
 	if (inp.size() > 1) p =  parseparams(inp[1]);
 
-	if (p.find("display") != p.end()) display=true;
 
 	printf("open... "); fflush(stdout);
 	if (!img.openFile(imageName, p)) err("File open failed.\n");
 	printf("done.\n"); fflush(stdout);
 	
-	if (argc == 2) {
-		printf("display... "); fflush(stdout);
-		img.displayImage();
-		printf("done.\n"); fflush(stdout);
-		return 0;
-	}
+	//if (argc == 2) {
+	//	printf("display... "); fflush(stdout);
+	//	img.displayImage();
+	//	printf("done.\n"); fflush(stdout);
+	//	return 0;
+	//}
 	
-	if (argc == 3 && std::string(argv[2]) == "metadata") {
-		img.printMetadata();
-		return 0;
-	}
+	//if (argc == 3 && std::string(argv[2]) == "metadata") {
+	//	img.printMetadata();
+	//	return 0;
+	//}
 
 	displayName = imageName;
-	cv::namedWindow(displayName);
+	if (display) cv::namedWindow(displayName);
 	if (display) img.displayImage(displayName);
 
 	//execute image ops:
-	for (unsigned i = 2; i < argc-1; i++) {
+	//for (unsigned i = 2; i < argc-1; i++) {
+	for (unsigned i = optind; i<argc-1; i++) {
 		std::vector<std::string> op = split(argv[i], ":");
 
 		if (op[0] == "blur") {
@@ -63,15 +141,29 @@ int main( int argc, char** argv )
 		else if (op[0] == "demosaic") {
 			printf("demosaic... "); fflush(stdout);
 			img.applyDemosaic();
-			if (display) img.displayImage();
+			if (display) img.displayImage(displayName);
 			printf("done.\n"); fflush(stdout);
 		}
 
 		else if (op[0] == "display") {
 			printf("display... "); fflush(stdout);
-			if (display) img.displayImage();
+			if (display) img.displayImage(displayName);
 			printf("done.\n"); fflush(stdout);
  		}
+		
+		else if (op[0] == "log") {
+			printf("log... "); fflush(stdout);
+			img.applyLog();
+			if (display) img.displayImage(displayName);
+			printf("done.\n"); fflush(stdout);
+		}
+		
+		else if (op[0] == "normalize") {
+			printf("normalize... "); fflush(stdout);
+			img.applyNormalization();
+			if (display) img.displayImage(displayName);
+			printf("done.\n"); fflush(stdout);
+		}
 
 		else if (op[0] == "resize") {
 			printf("resize... "); fflush(stdout);
